430011ce415f395133fee7f9921fc3f09c324040
John McLear <john@mclear.co.uk>
2021-01-17 15:12:25 +0000

    styling and loading msg

--- loading_message.js
+++ loading_message.js
@@ -6,10 +6,19 @@ exports.eejsBlock_loading = (hookName, args, cb) => {
     'Pads are automatically saved in real time',
     'The timeslider is a great way to see previous states of a pad',
     'Hundreds of plugins are available for Etherpad from the Plugin repository',
-    'To get mew plugins visit /admin/plugins',
+    'To get new plugins visit /admin/plugins',
     'The Etherpad foundation is a not for profit organization that maintains the Etherpad universe',
     'Etherpad is free and open source software, you can easily host your own Etherpad server',
     'Get your own free Etherpad download at http://etherpad.org',
+    'Thanks to @rasos for spotting some bugs in this plugin',
+    'Control and 5 will perform a strikethrough',
+    'Contol Z and Y will perform an undo/redo',
+    'Control Shift C will clear authorship colors',
+    'Tab can be used to indent content',
+    'Shift tab can be used to outdent(unindent) content',
+    'Control U will underline content',
+    'Control I will make content itallic',
+    'Thanks to the Translatewiki community Etherpad is available in all languages, even Klingon',
   ];
   const randomLoadingMsg = () => messages[Math.floor(Math.random() * messages.length)];
   args.content += `<div class='loading_message'>${randomLoadingMsg()}</div>`;
